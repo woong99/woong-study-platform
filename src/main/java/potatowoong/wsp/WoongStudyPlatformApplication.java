e8a8e64eb4e376b83a02ba60dbfef29dde857533
JeongWoongGyo <wg.jeong@springworks.co.kr>
2024-03-10 13:36:26 +0900

    first commit

---
+++ src/main/java/potatowoong/wsp/WoongStudyPlatformApplication.java
@@ -0,0 +1,13 @@
+package potatowoong.wsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoongStudyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoongStudyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/potatowoong/wsp/WoongStudyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package potatowoong.wsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoongStudyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
